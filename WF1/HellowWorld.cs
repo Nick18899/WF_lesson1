93ab2372fb2485927cb91c477605ac39965dc20c
Nick18899 <51851229+Nick18899@users.noreply.github.com>
2020-05-12 13:52:51 +0300

    Add files via upload

---
+++ WF1/HellowWorld.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF1
+{
+    public partial class HellowWorld : Form
+    {
+        public HellowWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormClosing += Form1_FormClosing;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "Hellow " + textBox1.Text + "!";
+            textBox2.Text = s;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
